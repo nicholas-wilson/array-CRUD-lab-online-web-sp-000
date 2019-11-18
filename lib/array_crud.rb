84a38ba105d1773a263af7b42a263b422face097
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 20:37:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = array.shift(element)
+  array = array.unshift(element)
   array
 end
 
@@ -24,7 +24,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = array.unshift
+  array = array.shift
   array
 end
 
